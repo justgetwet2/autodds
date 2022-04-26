1ada013f26cbf0bcdb76b20b1c8b947ddec8c464
AkiraMac <frogcherry7@gmail.com>
2022-04-26 16:49:35 +0900

    add buttons

--- tkmain.py
+++ tkmain.py
@@ -1,13 +1,25 @@
 
+import pickle
+import sys
 import tkinter as tk
 # from tkinter import ttk
+
+args = sys.argv 
+
+filepath = args[1]
+with open(filepath, mode="rb") as f:
+    races = pickle.load(f)
 
 root = tk.Tk()
 
 root.title("test")
-root.geometry("400x200+100+50")
+root.geometry("800x400+50+50")
 
 CheckBox = tk.Checkbutton(text=u"test")
-CheckBox.pack()
+# CheckBox.pack()
+
+for r in range(1, len(races)+1):
+    Button = tk.Button(text=f"{r}")
+    Button.pack(side="left", anchor=tk.NW)
 
 root.mainloop()
