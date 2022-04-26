25d5bc4fc98bdaf23aa0754f0b5313a77e24a4ee
AkiraMac <frogcherry7@gmail.com>
2022-04-26 17:08:19 +0900

    add button lamdba

--- tkmain.py
+++ tkmain.py
@@ -18,8 +18,11 @@ root.geometry("800x400+50+50")
 CheckBox = tk.Checkbutton(text=u"test")
 # CheckBox.pack()
 
+def tes(r):
+    print(r)
+
 for r in range(1, len(races)+1):
-    Button = tk.Button(text=f"{r}")
+    Button = tk.Button(text=f"{r}", command=lambda: tes(r))
     Button.pack(side="left", anchor=tk.NW)
 
 root.mainloop()
